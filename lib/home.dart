5e5a6534e336c77a25c70a06d88de77466d04498
Fabio Gabriel <fbogabriel@outlook.com>
2024-06-28 17:27:25 -0300

    Iniciada logica da classe Home

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,8 +9,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Inicio"),),
+      body: const Column(
+        children: [
+          TextButton(onPressed: Navigator.push(), child: Text("Entrar", style: TextStyle(fontSize: 20))),
+          TextButton(onPressed: onPressed, child: Text("Cadastrar", style: TextStyle(fontSize: 20))),
+          TextButton(onPressed: onPressed, child: Text("Pular", style: TextStyle(fontSize: 20))),
+        ],
+      ),
+    );
   }
 }
