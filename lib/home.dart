16539ca08f45214007216c3a1c512b6e5abf4b79
Fabio Gabriel <fbogabriel@outlook.com>
2024-06-29 01:10:36 -0300

    Mudanca na classe de login para usar Shared Preferencces (fazer persistencia no BD)

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+// ignore: depend_on_referenced_packages
+import "package:shared_preferences/shared_preferences.dart";
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +10,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String email = "";
+  String senha = "";
+
+  void _init() async{
+    final prefs = await SharedPreferences.getInstance();
+    bool hasData = prefs.containsKey("email");
+    //if(!hasData) 
+    //Caso nao tenha, vou ter que procurar no banco de dados, e enviar a outra tela de login!!!!
+  }
+
+  void _save() async{
+    final prefs = await SharedPreferences.getInstance();
+    
+    await prefs.setString("email", emailController.text);
+    await prefs.setString("senha", passwordController.text);
+    //await prefs.setString("isLogado", textController.text);
+
+  }
+  void _recover() async{
+    final prefs = await SharedPreferences.getInstance();
+    email = prefs.getString("isLogado")!;
+
+    setState((){});
+
+  }
+  void _remove() async{
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("isLogado");
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +49,9 @@ class _HomeState extends State<Home> {
       appBar: AppBar(title: const Text("Inicio"),),
       body: const Column(
         children: [
-          TextButton(onPressed: Navigator.push(), child: Text("Entrar", style: TextStyle(fontSize: 20))),
-          TextButton(onPressed: onPressed, child: Text("Cadastrar", style: TextStyle(fontSize: 20))),
-          TextButton(onPressed: onPressed, child: Text("Pular", style: TextStyle(fontSize: 20))),
+          //TextButton(onPressed: Navigator.push(), child: Text("Entrar", style: TextStyle(fontSize: 20))),
+          //TextButton(onPressed: onPressed, child: Text("Cadastrar", style: TextStyle(fontSize: 20))),
+          //TextButton(onPressed: onPressed, child: Text("Pular", style: TextStyle(fontSize: 20))),
         ],
       ),
     );
