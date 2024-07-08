3cc290d6ec6603e6b6952f023b4d00de23b258de
amandazirpolo <amandaszirpolo@gmail.com>
2024-07-08 12:20:18 -0300

    botoes dashboard ok

--- lib/cadastrar.dart
+++ lib/cadastrar.dart
@@ -13,107 +13,113 @@ class _CadastrarState extends State<Cadastrar> {
   TextEditingController mail = TextEditingController();
   TextEditingController password = TextEditingController();
 
-  void _save(context){
-    if(name.text.isEmpty && mail.text.isEmpty && password.text.isEmpty){
+  void _save(context) {
+    if (name.text.isEmpty && mail.text.isEmpty && password.text.isEmpty) {
       //Alert ...
-    }
-    else{//Confere primeiro se ninguem tem aquele email cadastrado
+    } else {
+      //Confere primeiro se ninguem tem aquele email cadastrado
 
       //object.email = "Email da Pessoa"
       //object.nome = "Nome da Pessoa"
       //Salvar no BD
     }
   }
+
+  bool checaCadastro() {
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Cadastrar",
-        style: TextStyle(
-          color: Colors.white
-        ),),
-      iconTheme: const IconThemeData(
-        color: Colors.white),
-      backgroundColor: const Color.fromARGB(255, 61, 2, 71),),
-      body: Center(
-        child: Column(
+        appBar: AppBar(
+          title: const Text(
+            "Cadastrar",
+            style: TextStyle(color: Colors.white),
+          ),
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+        ),
+        body: Center(
+            child: Column(
           children: [
-            const SizedBox(height: 50.0,),
-            SizedBox(
-              width: 350.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nome',
-                  labelStyle: const TextStyle(color: Colors.black),
-                  hintText: 'João da Silva',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(color: Colors.black, width: 2.0),
-                  ),
-                  filled: false,
-                  fillColor: Colors.grey[200],
-                ),
-                controller: name,
-              )
+            const SizedBox(
+              height: 50.0,
             ),
-            const SizedBox(height: 30.0,),
             SizedBox(
-              width: 350.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  labelStyle: const TextStyle(color: Colors.black),
-                  hintText: 'seunome@dominio.com',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(color: Colors.black, width: 2.0),
+                width: 350.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    hintText: 'João da Silva',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide:
+                          const BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    filled: false,
+                    fillColor: Colors.grey[200],
                   ),
-                  filled: false,
-                  fillColor: Colors.grey[200],
-                ),
-                controller: mail,
-              )
+                  controller: name,
+                )),
+            const SizedBox(
+              height: 30.0,
             ),
-            const SizedBox(height: 30.0,),
             SizedBox(
-              width: 350.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(color: Colors.black, width: 2.0),
+                width: 350.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    hintText: 'seunome@dominio.com',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide:
+                          const BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    filled: false,
+                    fillColor: Colors.grey[200],
                   ),
-                  filled: false,
-                  fillColor: Colors.grey[200],
-                ),
-                controller: password,
-                obscureText: true,
-              )
+                  controller: mail,
+                )),
+            const SizedBox(
+              height: 30.0,
             ),
-          //TextField(keyboardType:TextInputType.text, controller:name, decoration:const InputDecoration(labelText: "Digite seu nome"),),
-          //TextField(keyboardType: TextInputType.text, controller: mail, decoration:const InputDecoration(labelText: "Digite seu email"),),
-          //TextField(keyboardType: TextInputType.text, controller: password, decoration:const InputDecoration(labelText: "Digite sua senha"),obscureText: true,),
-         // ElevatedButton(onPressed: () => {_save(context)}, child: const Text("Salvar")),
-          const SizedBox(height: 55.0),
+            SizedBox(
+                width: 350.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide:
+                          const BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    filled: false,
+                    fillColor: Colors.grey[200],
+                  ),
+                  controller: password,
+                  obscureText: true,
+                )),
+            //TextField(keyboardType:TextInputType.text, controller:name, decoration:const InputDecoration(labelText: "Digite seu nome"),),
+            //TextField(keyboardType: TextInputType.text, controller: mail, decoration:const InputDecoration(labelText: "Digite seu email"),),
+            //TextField(keyboardType: TextInputType.text, controller: password, decoration:const InputDecoration(labelText: "Digite sua senha"),obscureText: true,),
+            // ElevatedButton(onPressed: () => {_save(context)}, child: const Text("Salvar")),
+            const SizedBox(height: 55.0),
             TextButton(
-              onPressed: () => {_save(context)},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 61, 2, 71),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12.0),
-                textStyle: const TextStyle(
-                  fontSize: 20.0
-                )
-              ),
-              child: const Text("Cadastrar",
-                style: TextStyle(
-                  color: Colors.white))
-            ),
+                onPressed: () => {_save(context)},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 28.0, vertical: 12.0),
+                    textStyle: const TextStyle(fontSize: 20.0)),
+                child: const Text("Cadastrar",
+                    style: TextStyle(color: Colors.white))),
           ],
-        )
-      )
-    );
+        )));
   }
 }
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,6 +10,95 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text(
+        "Dashboard",
+        style: TextStyle(color: Colors.white),
+      ),
+      iconTheme: const IconThemeData(color: Colors.white),
+      backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+    ),
+    body: Center(
+      child: Column(
+        children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    botaoAdicionar(context),
+                    const SizedBox(width: 20),
+                    botaoDeslogar(context),
+                    const SizedBox(width: 20),
+                    botaoFiltrar(context),
+                    const SizedBox(width: 15),
+                    botaoRecentes(context)
+                  ]
+                )
+              ],
+            ),
+        ],
+      )
+    ),);
   }
+}
+
+Widget botaoAdicionar(context) {
+  return ElevatedButton(
+    onPressed: () =>  Navigator.pushNamed(context, "novoJogo"),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      padding: EdgeInsets.zero,
+    ),
+    child: Image.asset('images/adicionar.png',
+      width: 60,
+      height: 58,));
+}
+
+Widget botaoDeslogar(context){
+  return ElevatedButton(
+    onPressed: () =>  Navigator.pop(context, "inicio"),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      padding: EdgeInsets.zero,
+    ),
+    child: Image.asset('images/deslogar.png',
+      width: 53,
+      height: 50,
+    )
+  );
+}
+
+Widget botaoFiltrar(context){
+  return ElevatedButton(
+    onPressed: () =>  Navigator.pushNamed(context, "filtrar"),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      padding: EdgeInsets.zero,
+    ),
+    child: Image.asset('images/filtrar.png',
+      width: 52,
+      height: 54,
+    )
+  );
+}
+
+Widget botaoRecentes(context){
+  return ElevatedButton(
+    onPressed: () =>  Navigator.pushNamed(context, "recentes"),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      padding: EdgeInsets.zero,
+    ),
+    child: Image.asset('images/recentes.png',
+      width: 58,
+      height: 58,
+    )
+  );
 }
