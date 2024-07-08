c535bbd1d498be6726c54d21b9d7b455ee267084
Fabio Gabriel <fbogabriel@outlook.com>
2024-07-08 12:48:49 -0300

    Adicionando as telas avaliacoes e detalhes

--- lib/avaliacoes.dart
+++ lib/avaliacoes.dart
@@ -8,8 +8,43 @@ class Avaliacoes extends StatefulWidget {
 }
 
 class _AvaliacoesState extends State<Avaliacoes> {
+  
+  Future<Map<String, Map<int, String>>> getAvaliacoes() async{
+    Map<String, Map<int, String>> avaliacoes = await {};
+
+    //Logica do Banco de Dados
+    avaliacoes.forEach((String a, dynamic lista){
+
+    });
+    return avaliacoes;
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: const Text("Avaliações"),),
+      body: Column(children: [
+        FutureBuilder(future: getAvaliacoes(), builder: (context, snapshot){
+            if(snapshot.hasError){
+              return Text("Erro");
+            }
+            else{
+
+              Map<String, Map>? avaliacoes = snapshot.data;
+              
+              return ListView.builder(itemBuilder: (context, index){
+                Map<String, int> avalia = avaliacoes![index];
+                return Row(children: [
+                  Text(index.toString()),
+                  Text(avalia[index])
+                ]);
+              });
+
+            }
+          })
+        
+        ,],
+      ),
+      );
+  
   }
 }
--- lib/detalhes.dart
+++ lib/detalhes.dart
@@ -8,8 +8,31 @@ class Detalhes extends StatefulWidget {
 }
 
 class _DetalhesState extends State<Detalhes> {
+  String descricao = "";
+  int? nota = null;
+  
+  void navAvaliacoes(context){
+    Navigator.pushNamed(context, "avaliacoes");
+  }
+
+  void navAvaliar(context){
+    Navigator.pushNamed(context, "avaliar");
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Detalhes"),),
+      body: Column(children: [
+        
+        Text(descricao),
+        
+        Text("Nota: ${nota.toString()}"),
+        
+        TextButton(onPressed: () => navAvaliacoes(context), child: const Text("Avaliacoes")),
+        
+        TextButton(onPressed: () => navAvaliar(context), child: const Text("Avaliar")),
+      ],),
+    );
   }
 }
