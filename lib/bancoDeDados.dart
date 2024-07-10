117de4b977d5d111e2a60ae688dc617e55517740
amandazirpolo <amandaszirpolo@gmail.com>
2024-07-09 22:15:25 -0300

    funcoes crud insere

--- lib/bancoDeDados.dart
+++ lib/bancoDeDados.dart
@@ -1,19 +1,104 @@
-import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-Future<Database> iniciaBanco() async {
-  String path = join(await getDatabasesPath(), 'gamesTracker.db');
-  const String scriptUser =
-      "CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL);";
-  const String scriptGenre =
-      "CREATE TABLE genre(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL);";
-  const String scriptGame =
-      "CREATE TABLE game(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, name VARCHAR NOT NULL UNIQUE, description TEXT NOT NULL, release_date VARCHAR NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id));";
+class BancoDados {
+  // atributo privado e estático do banco de dados
+  static final BancoDados instancia = BancoDados._();
+  static Database? banco; // variavel do banco de dados
 
-  return await openDatabase(path, version: 1, onCreate: (db, version) async {
-    await db.execute(scriptUser);
-    await db.execute(scriptGenre);
-    await db.execute(scriptGame);
-  });
+  // construtor privado
+  BancoDados._();
+
+  // construtor que retorna a unica instancia da classe
+  factory BancoDados() {
+    return instancia;
+  }
+
+  // metodo get banco
+  Future<Database> getBanco() async {
+    if (banco != null) return banco!;
+    banco = await iniciaBanco();
+    return banco!;
+  }
+
+  // configura a foreign key
+  Future<void> foreignKey(Database bd) async {
+    await bd.execute('PRAGMA foreign_keys = ON');
+  }
+
+  // metodo que inicia o banco de dados
+  Future<Database> iniciaBanco() async {
+    String caminho = join(await getDatabasesPath(), 'gamesTracker.db');
+    const String scriptUser =
+        "CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL);";
+    const String scriptGenre =
+        "CREATE TABLE genre(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL);";
+    const String scriptGame =
+        "CREATE TABLE game(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, name VARCHAR NOT NULL UNIQUE, description TEXT NOT NULL, release_date VARCHAR NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id));";
+    const String scriptGameGenre =
+        "CREATE TABLE game_genre(game_id INTEGER NOT NULL, genre_id INTEGER NOT NULL, FOREIGN KEY(game_id) REFERENCES game(id), FOREIGN KEY(genre_id) REFERENCES genre(id));";
+    const String scriptReview =
+        "CREATE TABLE review(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, game_id INTEGER NOT NULL, score REAL NOT NULL, description TEXT, date VARCHAR NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id), FOREIGN KEY(game_id) REFERENCES game(id));";
+
+    return await openDatabase(caminho, version: 1, onConfigure: foreignKey,
+        onCreate: (bd, version) async {
+      await bd.execute(scriptUser);
+      await bd.execute(scriptGenre);
+      await bd.execute(scriptGame);
+      await bd.execute(scriptGameGenre);
+      await bd.execute(scriptReview);
+    });
+  }
+
+  /* funções do crud */
+  Future<void> insereUser(String nome, String email, String password) async {
+    final bd = banco;
+    Map<String, dynamic> user = {
+      'name': nome,
+      'email': email,
+      'password': password
+    };
+    await bd?.insert('user', user,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> insereGenre(String nome) async {
+    final bd = banco;
+    Map<String, dynamic> genre = {'name': nome};
+    await bd?.insert('genre', genre,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> insereGame(
+      int userId, String name, String description, String release_date) async {
+    final bd = banco;
+    Map<String, dynamic> game = {
+      'user_id': userId,
+      'name': name,
+      'description': description,
+      'release_date': release_date
+    };
+    await bd?.insert('game', game,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> insereGameGenre(int gameId, int genreId) async {
+    final bd = banco;
+    Map<String, dynamic> gameGenre = {'game_id': gameId, 'genre_id': genreId};
+    await bd?.insert('game_genre', gameGenre,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> insereReview(int userId, int gameId, double score,
+      String description, String date) async {
+    final bd = banco;
+    Map<String, dynamic> review = {
+      'user_id': userId,
+      'game_id': gameId,
+      'score': score,
+      'description': description,
+      'date': date
+    };
+    await bd?.insert('review', review, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 }
--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,6 +1,8 @@
 // ignore: depend_on_referenced_packages
 import "package:shared_preferences/shared_preferences.dart";
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'BancoDeDados.dart';
 
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
@@ -13,25 +15,30 @@ class _InicioState extends State<Inicio> {
   String nome = "";
   String id = "";
 
-  void _init() async{
+  late Future<Database> bancoDados;
+
+  // essa função é responsável por criar o banco de dados
+  // void banco(){
+  //   bancoDados = iniciaBanco();
+  // }
+
+  void _init() async {
     final prefs = await SharedPreferences.getInstance();
     //Testa se tem nome e id salvos
     bool hasData = prefs.containsKey("nome");
-    if(!hasData) hasData = prefs.containsKey("id");
+    if (!hasData) hasData = prefs.containsKey("id");
     //Se tem dado ele busca
-    if(hasData) _recover();
+    if (hasData) _recover();
   }
 
-
-  void _recover() async{
+  void _recover() async {
     final prefs = await SharedPreferences.getInstance();
     nome = prefs.getString("nome")!;
     id = prefs.getString("id")!;
-    setState((){});
+    setState(() {});
   }
 
-  
-  void alerta(BuildContext context, String title, String message){
+  void alerta(BuildContext context, String title, String message) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -51,31 +58,28 @@ class _InicioState extends State<Inicio> {
     );
   }
 
-  
-  void navEntrar(BuildContext context){
-    if(id == "") {
+  void navEntrar(BuildContext context) {
+    if (id == "") {
       Navigator.pushNamed(context, "entrar");
     } else {
       Navigator.pushNamed(context, "dashboard");
     }
   }
 
-  void navCadastrar(BuildContext context){
-    if(id == ""){
+  void navCadastrar(BuildContext context) {
+    if (id == "") {
       Navigator.pushNamed(context, "cadastrar");
-    }
-    else{
+    } else {
       String title = "Usuario ja identificado";
       String message = "Clique em no botão de Entrar";
       alerta(context, title, message);
     }
   }
 
-  void navPular(BuildContext context){
-    if(id == ""){
+  void navPular(BuildContext context) {
+    if (id == "") {
       Navigator.pushNamed(context, "dashboard");
-    }
-    else{
+    } else {
       Navigator.pushNamed(context, "dashboard");
     }
   }
@@ -84,73 +88,62 @@ class _InicioState extends State<Inicio> {
   Widget build(BuildContext context) {
     _init();
     return Scaffold(
-      appBar: AppBar(title: const Text("Games Tracker",
-        style: TextStyle(
-          color: Colors.white
-        ),),
-      backgroundColor: const Color.fromARGB(255, 61, 2, 71),),
+      appBar: AppBar(
+        title: const Text(
+          "Games Tracker",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+      ),
       body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 100.0),
-            Text("Seja bem vindo(a), $nome!",
-            style: const TextStyle(
-              fontSize: 35.0
-            ),),
-            const SizedBox(height: 90.0),
-            TextButton(
+          child: Column(
+        children: [
+          const SizedBox(height: 100.0),
+          Text(
+            "Seja bem vindo(a), $nome!",
+            style: const TextStyle(fontSize: 35.0),
+          ),
+          const SizedBox(height: 90.0),
+          TextButton(
               onPressed: () => navEntrar(context),
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 61, 2, 71),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-                textStyle: const TextStyle(
-                  fontSize: 20.0
-                )
-              ),
-              child: const Text("Entrar",
-                style: TextStyle(
-                  color: Colors.white))
-            ),
-            const SizedBox(height: 40.0),
-            TextButton(
+                  backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 16.0),
+                  textStyle: const TextStyle(fontSize: 20.0)),
+              child:
+                  const Text("Entrar", style: TextStyle(color: Colors.white))),
+          const SizedBox(height: 40.0),
+          TextButton(
               onPressed: () => navCadastrar(context),
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 61, 2, 71),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-                textStyle: const TextStyle(
-                  fontSize: 20.0
-                )
-              ),
+                  backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 16.0),
+                  textStyle: const TextStyle(fontSize: 20.0)),
               child: const Text("Cadastrar",
-                style: TextStyle(
-                  color: Colors.white))
-            ),
-            const SizedBox(height: 40.0),
-            TextButton(
+                  style: TextStyle(color: Colors.white))),
+          const SizedBox(height: 40.0),
+          TextButton(
               onPressed: () => navPular(context),
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 61, 2, 71),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-                textStyle: const TextStyle(
-                  fontSize: 20.0
-                )
-              ),
-              child: const Text("Pular",
-                style: TextStyle(
-                  color: Colors.white))
-            ),
-          ],
-        )
-      ),
+                  backgroundColor: const Color.fromARGB(255, 61, 2, 71),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 16.0),
+                  textStyle: const TextStyle(fontSize: 20.0)),
+              child:
+                  const Text("Pular", style: TextStyle(color: Colors.white))),
+        ],
+      )),
     );
   }
 }
